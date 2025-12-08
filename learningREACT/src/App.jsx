2e740c1652b728e14ac123cd7aa80ecc4bf88522
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-08 19:08:13 +0530

    React

---
+++ learningREACT/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>    // React Fragment
+    <div id='dad'>
+      <h1 id='child1'>Hello</h1>
+      <h2 id="child2">Hello</h2>
+    </div>
+    <div id='mom'>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
