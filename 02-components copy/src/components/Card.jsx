b9e36eea97eec4824d10ff56b61e91fa0c5fcd60
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-09 20:34:47 +0530

    Initial commit: ract, 02-components, 03-props

---
+++ 02-components copy/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="text-2xl bg-cyan-200 p-5 m-5 rounded-xl">
+        <h2 className="font-bold">Baibhav Pratap Singh - From Card</h2>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+          facere?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
