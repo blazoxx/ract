740fa8b376443474f8dd865aaff36c2391eb6f4c
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-14 23:01:37 +0530

    13 completed

--- 13-Local-Storage/src/App.jsx
+++ 13-Local-Storage/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // localStorage.clear()      // to clear storage
+
+  // sessionStorage.clear()    // to clear storage
+
+  // localStorage.setItem("user", "BPS");   // to set item in storage
+
+  // const user = localStorage.getItem("user");  // to get item from storage
+
+  // localStorage.removeItem("user")  // to remove specific item from storage
+
+
+
+  // // Storing Object in Local Storage
+  // const user2 = {
+  //   name: 'BPS',
+  //   age: 24,
+  //   city: "Delhi"
+  // }
+
+  // console.log(user2)
+
+  // localStorage.setItem("user2", JSON.stringify(user2))  // to store object, we need to convert it into string
+
+  const user2 = JSON.parse(localStorage.getItem("user2"))  // to get object, we need to parse it back to object
+
+  console.log(user2)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-900 text-red-200 h-screen flex items-center justify-center"></div>
+  );
+};
 
-export default App
+export default App;
