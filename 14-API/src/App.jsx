ea61e46504885990da2c51613a7f847282eac8ff
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-15 08:05:58 +0530

    14 added

--- 14-API/src/App.jsx
+++ 14-API/src/App.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div className='text-red-200'>
-      dbuekansl;
-    </div>
-  )
-}
+  // use data
+  const [data, setData] = useState([]);
 
-export default App
+  // Incorrect: Missing await to handle the promise returned by fetch and response.json()
+  // function getData() {
+  //   const response = fetch("https://jsonplaceholder.typicode.com/users/1");
+
+  //   console.log(response);
+  // }
+
+  // // Corrected: Using async/await to properly handle the asynchronous fetch call
+  // async function asyGetData() {
+  //   const response = await fetch(
+  //     "https://jsonplaceholder.typicode.com/users/1"
+  //   );
+  //   const data = await response.json();
+  //   // console.log(response)
+  //   console.log(data);
+  // }
+
+  // // by axios
+  // const axiosGet = () => {
+  //   const response3 = axios.get("https://jsonplaceholder.typicode.com/users/1");
+
+  //   console.log(response3);
+  // };
+
+  const axiosGetData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+    // console.log(data);
+  };
+
+  return (
+    <div className="text-red-200 p-5 space-y-3 space-x-3">
+      {/* <button onClick={getData}>Get Data</button>
+      <button onClick={asyGetData}>Get Async Data</button>
+      <button onClick={axiosGet}>Get Axios Data</button> */}
+      <button onClick={axiosGetData}>Get Async Axios Data</button>
+
+      <div>
+        {data.map(function (elem,index) {
+          return <h3>{index} Hello {elem.author}</h3>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
