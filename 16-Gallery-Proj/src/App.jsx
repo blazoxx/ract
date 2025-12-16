21624f7d6a39e844fbe8474577b4c59d2e7cdda6
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-16 17:39:43 +0530

    16 initiated

---
+++ 16-Gallery-Proj/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <button className='bg-green px-5 py-2 rounded m-4'>Get Data</button>
+    </div>
+  )
+}
+
+export default App
